26b51086853212348e9c663951cc51ccd01f0f85
Tung Nguyen <tongueroo@gmail.com>
2019-02-02 21:28:59 +0000

    cleanup

--- lib/jets/builders/templates/handler.rb
+++ lib/jets/builders/templates/handler.rb
@@ -2,11 +2,6 @@ require "bundler/setup"
 require "jets"
 Jets.once  # runs once in lambda execution context
 
-<% @vars.functions.each do |function_name| -%>
-Jets.handler(self, "<%= @vars.handler_for(function_name) %>")
-<% end %>
-
-
 <% @vars.functions.each do |function_name|
   handler = @vars.handler_for(function_name)
   meth = handler.split('.').last
